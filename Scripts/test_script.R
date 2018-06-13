8433c7539cbe34be2c2aae277616d5f2341649ee
erclambr <ellen.lambrecht@ugent.be>
2018-06-13 14:47:22 +0200

    ggplot

--- Scripts/test_script.R
+++ Scripts/test_script.R
@@ -71,4 +71,49 @@ View(surveys_complete)
 
 ggplot(data=surveys_complete) # er gebeurt niks omdat ggplot extra info nodig heeft wat te plotten
 ggplot(data=surveys_complete, aes(x=weight, y= hindfoot_length))  #aes= aestethics
-ggplot(data=surveys_complete, aes(x=weight, y= hindfoot_length)) + geom_point()
+ggplot(data=surveys_complete, aes(x=weight, y= hindfoot_length)) + geom_point(alpha=0.1, aes(color= species_id))
+
+## scatter plot weight y axis, id (x axs), dot = plot type
+
+
+# boxplot met individuele datapunten (jitter)
+ggplot(data=surveys_complete, aes(x=species_id, y= weight)) + geom_boxplot() + geom_jitter(alpha=0.3, color= "tomato")
+
+ggplot(data=surveys_complete, aes(x=species_id, y= weight)) + geom_jitter(alpha=0.3, aes(color= plot_id))+ geom_boxplot()
+
+surveys_complete$plot_id <-factor(surveys_complete$plot_id)
+
+ggplot(data=surveys_complete, aes(x=species_id, y= weight)) + geom_violin(aes(color=plot_type))
+ggplot(data=surveys_complete, aes(x=species_id, y= weight)) + geom_violin() + scale_y_log10()
+
+ggplot(data=surveys_complete, aes(x=species_id, y= hindfoot_length)) + geom_boxplot() + geom_jitter(alpha=0.3, aes(color= plot_id))
+
+
+yearly_count <-surveys_complete %>%
+  group_by(year, species_id) %>%
+  tally()
+ggplot(data= yearly_count, aes(x=year, y=n, color=species_id)) +
+ geom_line()
+ 
+ggplot(data=yearly_count, aes (x=year, y=n)) + geom_line() + facet_wrap(~species_id)   # facet_wrap= aparte grafieken voor de species
+
+yearly_sex_counts<- surveys_complete %>%
+  group_by(year, species_id, sex) %>%
+  tally()
+
+ggplot(data=yearly_sex_counts, aes(x=year, y=n, color=sex)) + geom_line()+facet_wrap(~species_id)
+
+
+# achtergrond wit maken
+myplot<-ggplot(data=yearly_sex_counts, aes(x=year, y=n, color=sex)) + 
+  geom_line() +
+  facet_wrap(~species_id) + 
+  theme_bw() +
+  theme(panel.grid = element_blank(),
+          text=element_text(size=16),
+        axis.text.x= element_text(color="grey20", size= 12, angle=90, hjust=0.5, vjust=0.5))
+  labs(title="observed species over time", x="Year of observation", y= "Number of observations")
+                                                                                                                                                          ")
+ggsave("plots/my_first_plot.png", myplot, with= 15, hight=10, dpi=300)
+
+
