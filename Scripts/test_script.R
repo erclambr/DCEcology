dd341524a3c95f7fe676c5349fde434010201354
erclambr <ellen.lambrecht@ugent.be>
2018-06-13 12:33:33 +0200

    added ggplot

--- Scripts/test_script.R
+++ Scripts/test_script.R
@@ -44,5 +44,26 @@ str(surveys)
 
 
 install.packages("lubridate")
-libray(lubridate)
-install.packages("stringi")
+library(lubridate)
+
+
+my_date <-ymd("2015-01-01")  # datum weergeven op 2 verschillende manieren
+my_date <-ymd(paste("2015","1","1", sept="-"))
+# datum weergeven van verschillende kolommen
+paste(surveys$year, surveys$month, surveys$day, sep="-")
+ymd(paste(surveys$year, surveys$month, surveys$day, sep="-")) # aangeven dat het datums zijn
+surveys$data <-ymd(paste(surveys$year, surveys$month, surveys$day, sep="-")) # nieuwe kolom toevoegen aan surveys
+
+is_missing_date<- is.na(surveys$data)
+date_columns <-c("year", "month","day") # kies welke kolommen je wil weergeven later
+missing_dates <- surveys[is_missing_date, date_columns]
+head(missing_dates)
+
+
+
+
+
+download.file("https://figshare.com/s/fe0cd1848e06456e6f38","data/surveys_complete.csv")
+
+
+
